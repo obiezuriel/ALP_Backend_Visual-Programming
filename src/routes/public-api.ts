2fdddf9875ca1ca4ea2f4b96fd4343ebb4c8748a
Alvaro Grevi Kumesan <grevikumesan04@gmail.com>
2025-12-14 15:59:42 +0700

    feature: nambahin POST mood endpoint

--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { JournalController } from '../controllers/journal-controller';
+import { MoodController } from '../controllers/mood-controller';
 
 export const publicRouter = express.Router();
 
 publicRouter.post("/journal", JournalController.createJournal);
 publicRouter.get("/journal", JournalController.getAllJournals);
 publicRouter.get("/journal/:id", JournalController.getJournalById);
+publicRouter.post("/mood", MoodController.create);
