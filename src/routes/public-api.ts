7f25711bebe39e9ccf90a56f4ac9f65007a1d4cc
Chelsea Deanna Husin <chelseadh07@gmail.com>
2025-12-14 18:07:39 +0700

    add meditation backend

--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { JournalController } from '../controllers/journal-controller';
+import { MeditationController } from '../controllers/meditation-controller';
+
 
 export const publicRouter = express.Router();
 
 publicRouter.post("/journal", JournalController.createJournal);
 publicRouter.get("/journal", JournalController.getAllJournals);
 publicRouter.get("/journal/:id", JournalController.getJournalById);
+
+// Meditation routes
+publicRouter.get("/meditations", MeditationController.getAll);
+publicRouter.get("/meditations/:id", MeditationController.getById);
